b0bda594895489cfe8553ae21fa66a5c4127a814
DESKTOP-E5ICRGP\Administrator <sunwei3160@163.com>
2021-02-24 15:09:51 +0800

    fisrt

---
+++ src/main/java/com/test/undertow/underTowApplication.java
@@ -0,0 +1,17 @@
+package com.test.undertow;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author sw
+ * @version 1.0
+ * @company:LHCC
+ * @date 2021-02-24 15:06
+ */
+
+public class underTowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(underTowApplication.class);
+    }
+}
